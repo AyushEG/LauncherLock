cf4d936d3ba8a61c8471bc5c14f02ca96ae3fdb3
AYUSH-DELL\Ayush-EG <egayush@gmail.com>
2018-04-09 18:42:19 -0500

    [STABLE UPDATE] Snackbar completed, added a published date in the credits. Usable, unlike the last commit.

--- app/src/main/java/com/ayusheg/launcherlock/mainScreenActivity.java
+++ app/src/main/java/com/ayusheg/launcherlock/mainScreenActivity.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
@@ -40,6 +42,24 @@ public class mainScreenActivity extends AppCompatActivity {
 
         AdRequest adRequest = new AdRequest.Builder()
                 .setRequestAgent("android_studio:ad_template").build();
+
+        coordinatorLayout = (CoordinatorLayout)findViewById(R.id.coordinatorLayout);
+
+        checkBox = (CheckBox) findViewById(R.id.checkBox);
+
+        checkBox.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                final Snackbar snackbar = Snackbar.make(coordinatorLayout, "Lock Adjusted", Snackbar.LENGTH_LONG);
+                snackbar.setAction("Dismiss", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        snackbar.dismiss();
+                    }
+                });
+                snackbar.show();
+                }
+            });
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -61,41 +81,6 @@ public class mainScreenActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
-
-    public class MainActivity extends AppCompatActivity {
-        View.OnClickListener mOnClickListener;
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main_screen);
-
-            final CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id
-                    .coordinatorLayout);
-            final CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);
-            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    if (isChecked) {
-                        Snackbar snackbar = Snackbar
-                                .make(coordinatorLayout, "LauncherLock Active", Snackbar.LENGTH_LONG)
-                                .setAction("Undo", mOnClickListener);
-                        snackbar.setActionTextColor(Color.GREEN);
-                        View snackbarView = snackbar.getView();
-                        snackbarView.setBackgroundColor(Color.DKGRAY);
-                        TextView textView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
-                        textView.setTextColor(Color.YELLOW);
-                        snackbar.show();
-                    }
-                }
-            });
-
-            mOnClickListener = new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    checkBox.setChecked(false);
-                }
-            };
-        }
-    }
+    private CoordinatorLayout coordinatorLayout;
+    protected Button checkBox;
 }
